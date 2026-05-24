5c568f6b8bb7af3f1a03a85103d802c8266ecaa9
Erika Pacheco <enath6.25@gmail.com>
2026-05-24 14:30:36 -0600

    added image cover display

--- lib/main.dart
+++ lib/main.dart
@@ -14,45 +14,63 @@ class MyApp extends StatelessWidget {
       title: 'Movie App',
       theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
-      home:  Welcome(),
+      home: Welcome(),
     );
   }
 }
 
 //layout
-class Welcome extends StatelessWidget{
-const Welcome({super.key});
-@override
-Widget build(BuildContext context){
-  return const Column(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Text(
-        'Buenas pelis',
-        style: TextStyle(
-          fontSize: 38,
-          color: Colors.amberAccent,
-          letterSpacing: 2,
-        ),
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  Widget welcomeText(){
+    return const Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          'Buenas pelis',
+          style: TextStyle(
+            fontSize: 38,
+            color: Colors.amberAccent,
+            letterSpacing: 2,
+          ),
         ),
         SizedBox(height: 12),
         Text(
-'El lugar donde estan las mejores pelis',
-textAlign: TextAlign.center,
-style: TextStyle(
-  fontSize: 16,
+          'El lugar donde estan las mejores pelis',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 16,
             color: Colors.white70,
             fontStyle: FontStyle.italic,
-),
+          ),
+        ),
+      ],
+    );
+  }
 
-        )
 
-  ],
-  );
 
-}
+@override
+Widget build(BuildContext context){
+  const String urlBackgroundCine ='https://images.pexels.com/photos/18501410/pexels-photo-18501410.jpeg?_gl=1*3ua68b*_ga*NjI1NTgxNzQ5LjE3NzkwNDEwMDY.*_ga_8JE65Q40S6*czE3Nzk2NTM1NjQkbzQkZzEkdDE3Nzk2NTM5OTgkajYwJGwwJGgw';
+return Scaffold(
+  body: Stack(
+    children: [
+      Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+          image: NetworkImage(urlBackgroundCine),
+          fit: BoxFit.cover
+          )
+        ),
+      )
+    ],
+  ),
 
-}
+);
+
+}}
 
 //HOME widgets
 class HomeMovieScreen extends StatelessWidget {
@@ -65,70 +83,61 @@ class HomeMovieScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'Movies Home',
-        style: TextStyle(color: Colors.blueAccent, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-      backgroundColor: Colors.white,
-      elevation: 0,
+        backgroundColor: Colors.white,
+        elevation: 0,
       ),
       body: SingleChildScrollView(
-       child:Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: const Text(
-              'Estrenos',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Stack(
-                alignment: Alignment.topRight,
-                children: [
-                  Container(
-                    width:100,
-                    height: 150,
-                    color: Colors.grey[300],
-                  ),
-                  Container(
-                    color:Colors.blue,
-                     padding: const EdgeInsets.symmetric(horizontal:4),
-                     child: const Text(
-                      'Nuevo',
-                      style: TextStyle(color: Colors.white, fontSize: 12)
-                     ),
-                  ),
-                ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              child: const Text(
+                'Estrenos',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              Container(width: 100, height: 150, color: Colors.grey[300]),
-              Container(width: 100, height: 150, color: Colors.grey[300]),
-          ],)
-       ],)
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Stack(
+                  alignment: Alignment.topRight,
+                  children: [
+                    Container(width: 100, height: 150, color: Colors.grey[300]),
+                    Container(
+                      color: Colors.blue,
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: const Text(
+                        'Nuevo',
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(width: 100, height: 150, color: Colors.grey[300]),
+                Container(width: 100, height: 150, color: Colors.grey[300]),
+              ],
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.blueAccent,
         currentIndex: 0, //deja prendido el icono de home
         items: const [
-           BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
             label: 'Favoritos',
           ),
-         BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Buscador',
-          ),
-
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscador'),
         ],
       ),
-
     );
-
   }
 }
-
